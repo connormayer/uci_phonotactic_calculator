f63660f60f1e75908c82de789eecc541b2a25830
Arya K <aryarksub@gmail.com>
2022-04-27 14:30:05 -0700

    Change datasets page URL to /datasets

--- webcalc/urls.py
+++ webcalc/urls.py
@@ -3,7 +3,7 @@ from .views import UploadTrainView, MediaView, OutputView, AboutView
 
 urlpatterns = [
     path('', UploadTrainView.as_view(), name='home'),
-    path('media', MediaView.as_view(), name='media'),
+    path('datasets', MediaView.as_view(), name='media'),
     path('output', OutputView.as_view(), name='output'),
     path('about', AboutView.as_view(), name='about')
 ]
