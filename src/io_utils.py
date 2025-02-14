742a1565364fbe541c40aeffbd91de406756cc24
Austin Wagner <austinwagner@msn.com>
2025-02-14 04:07:08 -0800

    Update read_tokens: use default frequency of 1 when not provided

--- src/io_utils.py
+++ src/io_utils.py
@@ -1,6 +1,6 @@
 """
 io_utils.py - Utility module for I/O operations: reading tokens from a CSV file and writing results.
-Version: 1.0.1
+Version: 1.0.2
 """
 
 import csv
@@ -10,20 +10,22 @@ WORD_BOUNDARY = '#'
 
 def read_tokens(dataset):
     """
-    Reads in a file containing tokens and optional frequencies and converts
-    it to a list of tokens and a list of token/frequency pairs.
+    Reads in a file containing tokens and optional frequencies, and converts it to a list of token/frequency pairs.
 
-    dataset: The path to the dataset.
+    Parameters:
+      dataset (str): The path to the dataset.
 
-    returns: A list of lists, where each sublist corresponds to a token and
-             consists of a list of the individual symbols, plus a frequency if provided.
+    Returns:
+      list: A list of lists, where each sublist corresponds to a token and consists of:
+            - A list of the individual symbols (token).
+            - A frequency (float). If no frequency is provided in the CSV row, a default value of 1 is used.
     """
     with open(dataset, 'r', encoding='utf-8') as f:
         reader = csv.reader(f)
         token_freqs = []
         for row in reader:
             split_token = row[0].split(' ')
-            freq = float(row[1]) if len(row) == 2 else 0  # Default frequency is 0
+            freq = float(row[1]) if len(row) == 2 else 1  # Default frequency is 1 if not provided
             token_freqs.append([split_token, freq])
     return token_freqs
 
@@ -31,10 +33,12 @@ def write_results(results, outfile):
     """
     Writes the results of scoring the test dataset to a CSV file.
 
-    results: The list of result rows to write.
-    outfile: The path to the output CSV file.
+    Parameters:
+      results (list): The list of result rows to write.
+      outfile (str): The path to the output CSV file.
 
-    returns: None
+    Returns:
+      None
     """
     results = [HEADER] + results
     with open(outfile, 'w', encoding='utf-8', newline='') as f:
