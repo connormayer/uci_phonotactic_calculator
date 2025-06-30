87fd25aecfc22f7963519395fda7b9886d645021
wagner-austin <austinwagner@msn.com>
2025-06-29 17:17:19 -0700

    refactor(django): Refactor models and fix test imports

--- src/uci_phonotactic_calculator/web/django/webcalc/tests.py
+++ src/uci_phonotactic_calculator/web/django/webcalc/tests.py
@@ -3,13 +3,13 @@
 from django.test import TestCase
 from django.urls import reverse
 
-from uci_phonotactic_calculator import ngram_calculator
+from uci_phonotactic_calculator.cli import legacy
 
 
 class WebcalcViewTests(TestCase):
     """Test the webcalc views."""
 
-    def test_index_view(self):
+    def test_index_view(self) -> None:
         """Test that the index view returns a 200 status code."""
         response = self.client.get(reverse("webcalc:index"))
         self.assertEqual(response.status_code, 200)
@@ -18,6 +18,6 @@ class WebcalcViewTests(TestCase):
 class NgramCalculatorTests(TestCase):
     """Test the integration with the ngram_calculator module."""
 
-    def test_ngram_calculator_import(self):
+    def test_ngram_calculator_import(self) -> None:
         """Test that we can import the ngram_calculator module."""
-        self.assertIsNotNone(ngram_calculator)
+        self.assertIsNotNone(legacy)
